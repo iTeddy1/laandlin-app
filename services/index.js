86beefcf3621d218724246f55bbc6dfebc394d55
iTeddy1 <duytrung.ng1@gmail.com>
2025-06-10 23:49:59 +0700

    remove common service and update exports in index.js

--- services/index.js
+++ services/index.js
@@ -1,6 +1,5 @@
 export * from './user.service'
 export * from './product.service'
-export * from './common.service'
 export * from './category.service'
 export * from './review.service'
 export * from './order.service'
